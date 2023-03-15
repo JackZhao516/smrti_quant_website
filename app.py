0bccacd35c8a4f9ae3bdea2ef55a6b56611c7949
Jack Zhao <jiazhenz@umich.edu>
2023-03-14 22:28:52 -0400

    past 90 day kline plot website, beta (version 0.1)

---
+++ app.py
@@ -0,0 +1,60 @@
+
+import os
+import glob
+
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from crawl_coingecko import get_symbol_id_dict, plot_past_90_days_klines
+
+app = Flask(__name__)
+app.config['PLOTS'] = 'static'
+
+
+@app.route('/')
+def index():
+    print('Request for index page received')
+    return render_template('index.html')
+
+
+@app.route('/queryList', methods=['POST'])
+def query_list():
+    folder = app.config['PLOTS']
+    name = request.form.get('name')
+    symbol_2_id = get_symbol_id_dict()
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    else:
+        files = glob.glob(f'{folder}/*')
+        for f in files:
+            os.remove(f)
+
+    if not name:
+        return error_handler('No name provided')
+    name = name.strip().split(' ')
+    for n in name:
+        n = n.lower()
+        if n not in symbol_2_id:
+            return error_handler('No such coin: ' + n)
+
+    for n in name:
+        n = n.lower()
+        plot_past_90_days_klines([[symbol_2_id[n], n]], folder)
+
+    files = os.listdir(folder)
+
+    return render_template('plot.html', plots=files)
+
+
+@app.route('/uploads/<filename>')
+def get_img(filename):
+    """Get static file."""
+    return send_from_directory(
+        app.config['PLOTS'], filename)
+
+
+def error_handler(error_message):
+    return f"<html>{error_message}</html>"
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ crawl_coingecko.py
@@ -0,0 +1,56 @@
+import os
+import json
+from datetime import datetime
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from pycoingecko import CoinGeckoAPI
+
+cg = CoinGeckoAPI()
+matplotlib.use('agg')
+
+
+def get_symbol_id_dict():
+    coins = cg.get_coins_list()
+    symbol_id_dict = {}
+    for coin in coins:
+        symbol_id_dict[coin['symbol']] = coin['id']
+    return symbol_id_dict
+
+
+def plot_past_90_days_klines(coins, plot_dir):
+    if not os.path.exists(plot_dir):
+        os.mkdir(plot_dir)
+    for coin_id, name in coins:
+        klines = cg.get_coin_market_chart_by_id(id=coin_id.lower(), vs_currency='usd', days=89, interval='hourly')
+        klines = klines['prices']
+
+        # save close price vs time plot to directory
+        x, y = [], []
+        for i, kline in enumerate(klines):
+            x.append(datetime.utcfromtimestamp(int(kline[0]) / 1000))
+            y.append(float(kline[1]))
+
+        my_dpi = 96
+        k = 4
+        plt.rc('xtick', labelsize=8 * k)
+        plt.rc('ytick', labelsize=8 * k)
+
+        fig, ax = plt.subplots(figsize=(1250 * k / my_dpi, 500 * k / my_dpi), dpi=my_dpi)
+
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))
+        ax.xaxis_date()
+        fig.autofmt_xdate()
+
+        plt.plot(x, y, linewidth=1.5)
+        plt.title(f'{name.upper()} price vs time', fontsize=10 * k)
+        plt.xlabel('Time 90 days ago to now', fontsize=10 * k)
+
+        plt.ylabel('Price in USD', fontsize=10 * k)
+        plt.savefig(os.path.join(plot_dir, f'{coin_id}.png'), dpi=my_dpi)
+        plt.clf()
+
+
+if __name__ == '__main__':
+    plot_past_90_days_klines([["bitcoin", "BTC"], ["ethereum", "ETH"]], 'plots')
